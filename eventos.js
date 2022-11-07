f59228f347774657d8475ddd45ba3f1eecd6bcaa
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-07 20:35:20 -0100

    eventos done

--- eventos.js
+++ eventos.js
@@ -1,2 +1,42 @@
+const myForm = document.querySelector('#my-form')
 const submitButton = document.querySelector('#submit-button')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
 
+const items = document.querySelector('.items')
+
+const body = document.querySelector('body')
+
+
+submitButton.addEventListener("click", function(e){
+    
+    e.preventDefault()
+    // console.log(e)
+    // console.log("clicked!")
+
+    // console.log(nameInput.value)
+
+    const nameValue = nameInput.value
+    const emailValue = emailInput.value
+
+    if(nameValue === "" || emailValue === ""){
+
+        return alert("Favor, preencha todos os campos")
+    }
+
+    myForm.style.background = "green"
+    items.firstElementChild.textContent = nameValue
+    items.children[1].textContent = emailValue
+    items.children[2].remove()
+    items.children[3].remove()
+
+    
+    body.style.background = "yellow"
+})
+
+
+// acessa o valor do input
+
+nameInput.addEventListener('change', function(e){
+    console.log(e.target.value)
+})
